e80ee67ee37125f265f61c043188c3285fddfec4
nayyyhaa <nehaguptang.ng@gmail.com>
2021-07-20 16:01:58 +0530

    add: clock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  let [time, setTime] = useState();
+  let [date, setDates] = useState();
+
+  let todayTime = () => {
+    setInterval(updateTimer,1000);
+  }
+  let updateTimer = () => {
+    let today = new Date();
+    let currDate = today.toLocaleDateString();
+    let currtime =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    setDates(currDate);
+    setTime(currtime);
+  };
+  useEffect(todayTime,[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Meditation App</h2>
+  Today: {date}, {time}
+      <button >Start Timer</button>
+      
     </div>
   );
 }
-
-export default App;
